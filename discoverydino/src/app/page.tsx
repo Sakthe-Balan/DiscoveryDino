ca3f859678fb219b6198fb5d9310608cb754aec9
Sakthe-Balan <sakthebalan2003@gmail.com>
2024-04-12 00:49:27 +0530

    merge conflicts  resolved

--- discoverydino/src/app/page.tsx
+++ discoverydino/src/app/page.tsx
@@ -1,5 +1,6 @@
-"use client"
-import Header from "@/components/header";
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client';
+import Header from '@/components/header';
 import Filter from '@/components/filter';
 import Card from "@/components/card";
 import React, { useState,useEffect} from 'react';
@@ -16,13 +17,11 @@ interface CardData {
   website: string;
   category: string[];
   additionalInfo: string;
-  scarpedLink:string;
+  scarpedLink: string;
   reviews?: any[];
- }
-
+}
 
 export default function Home() {
-
   // Provide initial data as part of the state
   const initialData: CardData[] = [];
 
@@ -32,47 +31,124 @@ export default function Home() {
  const [eexportdata, setEexportdata]:any = useState();
  const [showExportDialog, setShowExportDialog] = useState(false);
 
- const fetchData = async () => {
-  setIsLoading(true); // Start loading
-  try {
-     const response: any = await axios.get(`https://itnrmdjr7f.ap-south-1.awsapprunner.com/api/data?limit=${itemsToDisplay}`);
- 
-     // The response data is already parsed as JSON
-    //  console.log(response.data);
-     
-     // Directly use response.data since it's already an object
 
-     const newData = response.data;
+  
 
-     console.log('API Response:', newData);
-   
-     // Check if newData is an array before updating the state
-     if (Array.isArray(newData)) {
-      
-      setData(prevData => {
-        const existingIds = new Set(prevData.map(item => item._id));
-        const filteredNewData = newData.filter(item => !existingIds.has(item._id));
-        return [...prevData, ...filteredNewData];
-      });
-      setItemsToDisplay(prevItems => prevItems + 9);
-     }
-     else {
-     console.log("out");}
-  } catch (error) {
-     console.error('Error fetching data:', error);
-     // Optionally, handle the error case, such as showing an error message to the user
-  } finally {
-     setIsLoading(false); // End loading
-  }
- };
- 
- useEffect(() => {
+  const [parentSelectedRatings, setParentSelectedRatings] = useState<number[]>(
+    []
+  );
+  const [parentSelectedCategory, setParentSelectedCategory] = useState<
+    string | null
+  >(null);
+
+  useEffect(() => {
+    // Define a function to fetch filtered products based on selected ratings and category
+    const fetchFilteredProducts = async () => {
+      if (parentSelectedRatings.length === 0 && !parentSelectedCategory) {
+        try {
+          const response = await axios.get(
+            `${process.env.NEXT_PUBLIC_SERVER_URL}/api/data?limit=9`
+          );
+          const newData = response.data;
+
+          console.log('API Response:', newData);
+
+          // Check if newData is an array before updating the state
+          if (Array.isArray(newData)) {
+            // setData((prevData) => {
+            //   const existingIds = new Set(prevData.map((item) => item._id));
+            //   const filteredNewData = newData.filter(
+            //     (item) => !existingIds.has(item._id)
+            //   );
+            //   return [...prevData, ...filteredNewData];
+            // });
+            setData(newData);
+            setItemsToDisplay(18);
+            // setItemsToDisplay((prevItems) => prevItems + 9);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      } else {
+        // Construct API URL based on selected ratings and category
+        const queryParams = new URLSearchParams({
+          collection: 'filtered_products',
+        });
+
+        if (parentSelectedRatings.length > 0) {
+          queryParams.set('rating', parentSelectedRatings.join(','));
+        }
+
+        if (parentSelectedCategory) {
+          queryParams.set('category', parentSelectedCategory);
+        }
+
+        const apiUrl = `${
+          process.env.NEXT_PUBLIC_SERVER_URL
+        }/api/filter?${queryParams.toString()}`;
+
+        try {
+          const response = await axios.get(apiUrl);
+          const data = await response.data;
+          setData(data.results);
+          // Update state with filtered products
+          console.log(data.results);
+          // setData(data.results); // Assuming 'results' is the key containing filtered products
+        } catch (error) {
+          console.error('Error fetching filtered products:', error);
+        }
+      }
+    };
+    // Call fetchFilteredProducts whenever selected ratings or category change
+    fetchFilteredProducts();
+  }, [parentSelectedRatings, parentSelectedCategory]);
+
+  const fetchData = async () => {
+    setIsLoading(true); // Start loading
+    try {
+      console.log(`${process.env.NEXT_PUBLIC_SERVER_URL}`);
+
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/data?limit=${itemsToDisplay}`
+      );
+
+      // The response data is already parsed as JSON
+      //  console.log(response.data);
+
+      // Directly use response.data since it's already an object
+
+      const newData = response.data;
+
+      console.log('API Response:', newData);
+
+      // Check if newData is an array before updating the state
+      if (Array.isArray(newData)) {
+        setData((prevData) => {
+          const existingIds = new Set(prevData.map((item) => item._id));
+          const filteredNewData = newData.filter(
+            (item) => !existingIds.has(item._id)
+          );
+          return [...prevData, ...filteredNewData];
+        });
+        setItemsToDisplay((prevItems) => prevItems + 9);
+      } else {
+        console.log('out');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Optionally, handle the error case, such as showing an error message to the user
+    } finally {
+      setIsLoading(false); // End loading
+    }
+  };
+
+  useEffect(() => {
     fetchData();
- }, []); // Empty dependency array means this effect runs once on mount
+  }, []); // Empty dependency array means this effect runs once on mount
 
- const handleLoadMore = () => {
+  const handleLoadMore = () => {
     fetchData();
- };
+  };
 
 
  const handleExport = (exportDisplayedData: boolean) => {
@@ -134,23 +210,27 @@ const exportData = (exportedData: CardData[]) => {
         {/* Background stickers */}
         <div className="absolute top-0 right-0 bg-orange-500 h-20 w-20 rounded-full transform rotate-45"></div>
         <div className="absolute top-2 left-6 bg-yellow-500 h-12 w-12 rounded-lg transform rotate-45"></div>
-        
+
         {/* Filter section */}
         <div className="w-full md:w-[20%] md:border-r sm:mr-2">
-            <Filter />
+          <Filter
+            selectedRatings={parentSelectedRatings}
+            setSelectedRatings={setParentSelectedRatings}
+            selectedCategory={parentSelectedCategory}
+            setSelectedCategory={setParentSelectedCategory}
+          />
         </div>
 
         {/* Main content section */}
         <div className="md:w-[80%] flex-grow p-4 mt-4 mx-4 border-r-4 rounded-md shadow-lg bg-white border-gray-300  relative">
-          
           {/* Grid of cards */}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {data.map((item, index) => (
-              <Card 
+            {data.map((item, index) => (
+              <Card
                 key={index} // It's important to provide a unique key for each child in a list
-                heading={item.productName} 
-                photoUrl={item.photoUrl} 
-                description={item.description} 
+                heading={item.productName}
+                photoUrl={item.photoUrl}
+                description={item.description}
                 rating={item.rating}
                 similarProducts={item.similarProducts}
                 contactMail={item.contactMail}
@@ -172,6 +252,19 @@ const exportData = (exportedData: CardData[]) => {
               Load More
             </button>
           </div>
+          {/* Conditional rendering of loading indicator */}
+          {isLoading && (
+            <div className="mt-4 text-center">Loading more items...</div>
+          )}
+          {/* Load More Button */}
+          {parentSelectedRatings.length === 0 && !parentSelectedCategory && (
+            <button
+              onClick={handleLoadMore}
+              className="mt-4 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded w-full"
+            >
+              Load More
+            </button>
+          )}
         </div>
       </div>
 
